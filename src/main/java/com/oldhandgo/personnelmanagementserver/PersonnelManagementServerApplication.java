b704ccc703a010d01e8fafef643a63eb23c2d756
dormirr <1018729292@qq.com>
2019-10-08 14:48:07 +0800

    添加注释。
    Signed-off-by: dormirr <1018729292@qq.com>

--- src/main/java/com/oldhandgo/personnelmanagementserver/PersonnelManagementServerApplication.java
+++ src/main/java/com/oldhandgo/personnelmanagementserver/PersonnelManagementServerApplication.java
@@ -3,6 +3,11 @@ package com.oldhandgo.personnelmanagementserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dormir
+ * @date 2019/10/08
+ * @version 0.0.1
+ */
 @SpringBootApplication
 public class PersonnelManagementServerApplication {
 
