65c77b12c2269e7b00a874a7dedfc3f45e54fc2b
shiro <496735019@qq.com>
2019-10-08 16:18:53 +0800

    Emp
    Signed-off-by: shiro <496735019@qq.com>

---
+++ src/main/java/com/oldhandgo/personnelmanagementserver/domain/Emp.java
@@ -0,0 +1,34 @@
+package com.oldhandgo.personnelmanagementserver.domain;
+
+/**
+ * @author shiro
+ * @create 2019-10-08 16:11
+ */
+public class Emp {
+
+    private int id;
+
+    private String ename;//姓名
+
+    private String ex;//性别
+
+    private String edata;//出生日期
+
+    private String education;//学历
+
+    private String ephone;//手机
+
+    private String email;//邮箱
+
+    private String eadr;//住址
+
+    private String edepartment;//部门
+
+    private String eposition;//职位
+
+    private String erdata;//入职日期
+
+    private double salary;//薪水
+
+    private String hobby;//爱好
+}
