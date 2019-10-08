037ec5b8019a6204518f733d07a672ae32270f17
shiro <496735019@qq.com>
2019-10-08 14:21:23 +0800

    sout
    Signed-off-by: shiro <496735019@qq.com>

--- src/main/java/com/oldhandgo/personnelmanagementserver/PersonnelManagementServerApplication.java
+++ src/main/java/com/oldhandgo/personnelmanagementserver/PersonnelManagementServerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonnelManagementServerApplication {
 
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
         SpringApplication.run(PersonnelManagementServerApplication.class, args);
     }
 
