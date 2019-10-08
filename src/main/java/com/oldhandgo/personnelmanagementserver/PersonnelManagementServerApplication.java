fa920910fb2edc035facdcae36c7f90eb6b1cb4c
dormirr <1018729292@qq.com>
2019-10-08 13:41:17 +0800

    项目初始化。
    Signed-off-by: dormirr <1018729292@qq.com>

---
+++ src/main/java/com/oldhandgo/personnelmanagementserver/PersonnelManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.oldhandgo.personnelmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelManagementServerApplication.class, args);
+    }
+
+}
