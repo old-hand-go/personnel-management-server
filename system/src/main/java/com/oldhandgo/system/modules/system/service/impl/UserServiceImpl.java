aac1d9cd50087a04fc8ffcc259fc9264b46e7843
dormirr <1018729292@qq.com>
2019-10-10 19:07:52 +0800

    实现UserService接口中根据email查询的方法； 开启事务管理； 开启缓存。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/system/service/impl/UserServiceImpl.java
+++ system/src/main/java/com/oldhandgo/system/modules/system/service/impl/UserServiceImpl.java
@@ -1,38 +1,49 @@
-//package com.oldhandgo.system.modules.system.service.impl;
-//
-//import com.oldhandgo.common.utils.ValidationUtil;
-//import com.oldhandgo.system.modules.system.domain.User;
-//import com.oldhandgo.system.modules.system.repository.UserRepository;
-//import com.oldhandgo.system.modules.system.service.UserService;
-//import com.oldhandgo.system.modules.system.service.dto.UserDTO;
-//import com.oldhandgo.system.modules.system.service.mapper.UserMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * @author dormir
-// */
-//@Service
-//public class UserServiceImpl implements UserService {
-//    @Autowired
-//    private UserRepository userRepository;
-//    @Autowired
-//    private UserMapper userMapper;
-//
-//    @Override
-//    public UserDTO findById(long id) {
-//        return null;
-//    }
-//
-//    @Override
-//    public UserDTO findByUserName(String userName) throws Exception {
-//        User user = null;
-//        if (ValidationUtil.isEmail(userName)) {
-//            user = userRepository.findByEmail(userName);
-//        } else {
-//            throw new Exception();
-//        }
-//        return userMapper.userToUserDto(user);
-//
-//    }
-//}
+package com.oldhandgo.system.modules.system.service.impl;
+
+import com.oldhandgo.common.exception.EntityNotFoundException;
+import com.oldhandgo.common.utils.ValidationUtil;
+import com.oldhandgo.system.modules.system.domain.User;
+import com.oldhandgo.system.modules.system.repository.UserRepository;
+import com.oldhandgo.system.modules.system.service.UserService;
+import com.oldhandgo.system.modules.system.service.dto.UserDTO;
+import com.oldhandgo.system.modules.system.service.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author dormir
+ */
+@Service
+@CacheConfig(cacheNames = "users")
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true, rollbackFor = Exception.class)
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    @Cacheable(key = "#id")
+    public UserDTO findById(long id) {
+        return null;
+    }
+
+    @Override
+    @Cacheable(key = "#email")
+    public UserDTO findByEmail(String email) {
+        User user;
+        if (ValidationUtil.isEmail(email)) {
+            user = userRepository.findByEmail(email);
+        } else {
+            throw new EntityNotFoundException(User.class, "email", email);
+        }
+        return userMapper.userToUserDto(user);
+    }
+}
