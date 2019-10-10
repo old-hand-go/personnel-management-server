f3b3828f052eaacbf81508b800a27cbe6c9bb8ab
dormirr <1018729292@qq.com>
2019-10-10 19:03:35 +0800

    实现UserMapper通过依赖注入的方式实例化； 定义根据User生成UserDTO的方法。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/system/service/mapper/UserMapper.java
+++ system/src/main/java/com/oldhandgo/system/modules/system/service/mapper/UserMapper.java
@@ -2,20 +2,19 @@ package com.oldhandgo.system.modules.system.service.mapper;
 
 import com.oldhandgo.system.modules.system.domain.User;
 import com.oldhandgo.system.modules.system.service.dto.UserDTO;
-import org.mapstruct.MapMapping;
+
 import org.mapstruct.Mapper;
 
 /**
  * @author dormir
  */
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     /**
      * UserDTO映射
      *
-     * @param user
-     * @return
+     * @param user User实体类
+     * @return UserDTO映射
      */
-    @MapMapping
     UserDTO userToUserDto(User user);
 }
