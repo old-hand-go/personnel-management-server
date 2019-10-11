2df6071d644321faaff47bf867ffe99f97d886d1
dormirr <1018729292@qq.com>
2019-10-11 16:09:30 +0800

    优化验证码生成工具类。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/security/utils/VerifyCodeUtils.java
+++ system/src/main/java/com/oldhandgo/system/modules/security/utils/VerifyCodeUtils.java
@@ -52,13 +52,13 @@ public class VerifyCodeUtils {
     /**
      * 输出指定验证码图片流
      *
-     * @param width  创建图像的宽度
-     * @param height 创建图像的高度
-     * @param os
-     * @param code   验证码
+     * @param width        创建图像的宽度
+     * @param height       创建图像的高度
+     * @param outputStream
+     * @param code         验证码
      * @throws IOException
      */
-    public static void outputImage(int width, int height, OutputStream os, String code) throws IOException {
+    public static void outputImage(int width, int height, OutputStream outputStream, String code) throws IOException {
         int verifySize = code.length();
 
         /*
@@ -105,8 +105,8 @@ public class VerifyCodeUtils {
          */
         graphics.fillRect(0, 0, width, height);
 
-        Color color = getRandomColor(200, 250);
         // 设置背景色
+        Color color = getRandomColor(200, 250);
         graphics.setColor(color);
         /*
          * 填充指定的矩形
@@ -162,26 +162,86 @@ public class VerifyCodeUtils {
         // 使图片扭曲
         shear(graphics, width, height, color);
 
+        //设置字体颜色
         graphics.setColor(getRandomColor(100, 160));
+
+        //设置字体大小
         int fontSize = height - 4;
+
+        /*
+         * 设置图形的字体
+         *
+         * public Font​(String name, int style, int size)
+         * name 字体名称
+         * style 字体样式
+         * size 字体大小
+         *
+         * PLAIN 普通样式
+         * ITALIC 斜体样式
+         */
         Font font = new Font("Algerian", Font.ITALIC, fontSize);
         graphics.setFont(font);
+
         char[] chars = code.toCharArray();
         for (int i = 0; i < verifySize; i++) {
-            AffineTransform affine = new AffineTransform();
-            affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (width / verifySize) * i + fontSize / 2, height / 2);
-            graphics.setTransform(affine);
+            // 表示2D仿射变换，可以使用平移，缩放，翻转，旋转和剪切的序列来构造仿射变换
+            AffineTransform affineTransform = new AffineTransform();
+            /*
+             * 将此变换设置为平移旋转变换
+             *
+             * public void setToRotation​(double theta, double anchorx, double anchory)
+             * theta -旋转角度（以弧度为单位）
+             * anchorx -旋转锚点的X坐标
+             * anchory -旋转锚点的Y坐标
+             *
+             * 此操作等效于平移坐标，使锚点位于原点（S1），然后将其绕新原点旋转（S2），最后平移，以便将中间原点恢复到原始锚点的坐标（S3）
+             * 此操作等效于以下调用序列：
+             * setToTranslation(anchorx，anchory); S3：最终平移
+             * rotate(theta); S2：围绕锚点
+             * translate(-anchorx，-anchory); S1：将锚点转换为原点
+             *
+             * 表示此矩阵变为：
+             * [   cos(theta)    -sin(theta)    x-x*cos+y*sin  ]
+             * [   sin(theta)     cos(theta)    y-x*sin-y*cos  ]
+             * [       0              0               1        ]
+             */
+            affineTransform.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (width / verifySize) * i + fontSize / 2, height / 2);
+            graphics.setTransform(affineTransform);
+
+            /*
+             * 使用此图形的当前字体和颜色绘制由指定字符数组给定的文本
+             * 第一个字符的基线在此图形上下文的坐标系中的位置（x，y）
+             *
+             * public void drawChars​(char[] data, int offset, int length, int x, int y)
+             * data 要绘制的字符数组
+             * offset 数据中的起始偏移量
+             * length 要绘制的字符数
+             * x 文本基线的x坐标
+             * y 文本基线的y坐标
+             *
+             */
             graphics.drawChars(chars, i, 1, ((width - 10) / verifySize) * i + 5, height / 2 + fontSize / 2 - 10);
         }
-
+        // 处置此图形并释放它正在使用的所有系统资源
         graphics.dispose();
-        ImageIO.write(image, "jpg", os);
+        /*
+         * 使用ImageWriter支持给定格式的任意图像将图像写入OutputStream。
+         * 在写操作完成后，此方法不会关闭提供OutputStream的内容。如果需要，调用者有责任关闭流。
+         * 来自getUseCache和getCacheDirectory的当前缓存设置将用于控制缓存。
+         *
+         * public static boolean write​(RenderedImage im, String formatName, OutputStream output) throws IOException
+         * im 一个要写的RenderedImage
+         * formatName String包含格式的非正式名称
+         * output 要写入的OutputStream
+         */
+        ImageIO.write(image, "jpg", outputStream);
     }
 
     /**
      * 随机获取一种颜色
-     * @param fc
-     * @param bc
+     *
+     * @param fc R、G、B的最小值
+     * @param bc R、G、B的最大值
      * @return Color类型的颜色
      */
     private static Color getRandomColor(int fc, int bc) {
@@ -197,6 +257,11 @@ public class VerifyCodeUtils {
         return new Color(r, g, b);
     }
 
+    /**
+     * 随机获取一种颜色
+     *
+     * @return int型颜色
+     */
     private static int getRandomIntColor() {
         int[] rgb = getRandomRgb();
         int color = 0;
@@ -209,6 +274,7 @@ public class VerifyCodeUtils {
 
     /**
      * 随机获取R、G、B三个值
+     *
      * @return 保存R、G、B三个值的数组
      */
     private static int[] getRandomRgb() {
@@ -219,12 +285,12 @@ public class VerifyCodeUtils {
         return rgb;
     }
 
-    private static void shear(Graphics g, int w1, int h1, Color color) {
-        shearX(g, w1, h1, color);
-        shearY(g, w1, h1, color);
+    private static void shear(Graphics graphics, int width, int height, Color color) {
+        shearX(graphics, width, height, color);
+        shearY(graphics, width, height, color);
     }
 
-    private static void shearX(Graphics g, int w1, int h1, Color color) {
+    private static void shearX(Graphics graphics, int width, int height, Color color) {
 
         int period = random.nextInt(2);
 
@@ -232,38 +298,38 @@ public class VerifyCodeUtils {
         int frames = 1;
         int phase = random.nextInt(2);
 
-        for (int i = 0; i < h1; i++) {
+        for (int i = 0; i < height; i++) {
             double d = (double) (period >> 1)
                     * Math.sin((double) i / (double) period
                     + (6.2831853071795862D * (double) phase)
                     / (double) frames);
-            g.copyArea(0, i, w1, 1, (int) d, 0);
+            graphics.copyArea(0, i, width, 1, (int) d, 0);
             if (borderGap) {
-                g.setColor(color);
-                g.drawLine((int) d, i, 0, i);
-                g.drawLine((int) d + w1, i, w1, i);
+                graphics.setColor(color);
+                graphics.drawLine((int) d, i, 0, i);
+                graphics.drawLine((int) d + width, i, width, i);
             }
         }
 
     }
 
-    private static void shearY(Graphics g, int w1, int h1, Color color) {
+    private static void shearY(Graphics graphics, int width, int height, Color color) {
 
         int period = random.nextInt(40) + 10; // 50;
 
         boolean borderGap = true;
         int frames = 20;
         int phase = 7;
-        for (int i = 0; i < w1; i++) {
+        for (int i = 0; i < width; i++) {
             double d = (double) (period >> 1)
                     * Math.sin((double) i / (double) period
                     + (6.2831853071795862D * (double) phase)
                     / (double) frames);
-            g.copyArea(i, 0, 1, h1, 0, (int) d);
+            graphics.copyArea(i, 0, 1, height, 0, (int) d);
             if (borderGap) {
-                g.setColor(color);
-                g.drawLine(i, (int) d, i, 0);
-                g.drawLine(i, (int) d + h1, i, h1);
+                graphics.setColor(color);
+                graphics.drawLine(i, (int) d, i, 0);
+                graphics.drawLine(i, (int) d + height, i, height);
             }
 
         }
