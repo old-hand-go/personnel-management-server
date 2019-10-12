1bd449d69b4a776597337b242d7e6a75b36a2840
zhaoxiaoshuai <746185810@qq.com>
2019-10-12 11:31:56 +0800

    RBAC权限控制
    Signed-off-by: zhaoxiaoshuai <746185810@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/system/domain/Node.java
+++ system/src/main/java/com/oldhandgo/system/modules/system/domain/Node.java
@@ -1,13 +1,8 @@
 package com.oldhandgo.system.modules.system.domain;
 
 import lombok.Data;
-import lombok.ToString;
-
-import javax.persistence.Entity;
 import java.sql.Timestamp;
 
-@ToString
-@Entity
 @Data
 public class Node {
     private Long id;
