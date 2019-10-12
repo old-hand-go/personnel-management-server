b6a54f2038db73867924928518377bfc3c9a1b91
dormirr <1018729292@qq.com>
2019-10-12 15:04:47 +0800

    创建部门类； 创建角色类； 创建权限类。
    Signed-off-by: dormirr <1018729292@qq.com>

---
+++ system/src/main/java/com/oldhandgo/system/modules/system/domain/Permission.java
@@ -0,0 +1,106 @@
+package com.oldhandgo.system.modules.system.domain;
+
+import lombok.ToString;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+/**
+ * @author dormir
+ */
+@ToString
+@Entity
+public class Permission {
+    private Long id;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+    private String permissionName;
+    private String alias;
+    private Long pid;
+
+    @Id
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "create_time")
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "update_time")
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Basic
+    @Column(name = "permission_name")
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
+
+    @Basic
+    @Column(name = "alias")
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    @Basic
+    @Column(name = "pid")
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Permission that = (Permission) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(createTime, that.createTime) &&
+                Objects.equals(updateTime, that.updateTime) &&
+                Objects.equals(permissionName, that.permissionName) &&
+                Objects.equals(alias, that.alias) &&
+                Objects.equals(pid, that.pid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createTime, updateTime, permissionName, alias, pid);
+    }
+}
--- system/src/main/java/com/oldhandgo/system/modules/system/domain/Role.java
+++ system/src/main/java/com/oldhandgo/system/modules/system/domain/Role.java
@@ -1,27 +1,118 @@
 package com.oldhandgo.system.modules.system.domain;
 
-import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.sql.Timestamp;
+import java.util.Objects;
 
-@Data
+/**
+ * @author dormir
+ */
+@ToString
+@Entity
 public class Role {
+    private Long id;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+    private String roleName;
+    private String remark;
+    private String dataScope;
+    private Short level;
+
     @Id
     @Column(name = "id")
-    private Long id;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Basic
     @Column(name = "create_time")
-    private Timestamp createTime;
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
     @Basic
     @Column(name = "update_time")
-    private Timestamp updateTime;
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
     @Basic
-    @Column(name = "rolename")
-    private String rolename;//角色名称
+    @Column(name = "role_name")
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
     @Basic
-    @Column(name = "did")
-    private Long did;//所属部门
+    @Column(name = "remark")
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Basic
+    @Column(name = "data_scope")
+    public String getDataScope() {
+        return dataScope;
+    }
+
+    public void setDataScope(String dataScope) {
+        this.dataScope = dataScope;
+    }
+
+    @Basic
+    @Column(name = "level")
+    public Short getLevel() {
+        return level;
+    }
+
+    public void setLevel(Short level) {
+        this.level = level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) &&
+                Objects.equals(createTime, role.createTime) &&
+                Objects.equals(updateTime, role.updateTime) &&
+                Objects.equals(roleName, role.roleName) &&
+                Objects.equals(remark, role.remark) &&
+                Objects.equals(dataScope, role.dataScope) &&
+                Objects.equals(level, role.level);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createTime, updateTime, roleName, remark, dataScope, level);
+    }
 }
