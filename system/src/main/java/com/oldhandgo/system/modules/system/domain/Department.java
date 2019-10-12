bca0fcdcd03e4ef75a0ae6064b0cb8aec384e0df
dormirr <1018729292@qq.com>
2019-10-12 11:02:56 +0800

    部门实体类设计。
    Signed-off-by: dormirr <1018729292@qq.com>

---
+++ system/src/main/java/com/oldhandgo/system/modules/system/domain/Department.java
@@ -0,0 +1,102 @@
+package com.oldhandgo.system.modules.system.domain;
+
+import lombok.ToString;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+/**
+ * @author dormir
+ */
+@ToString
+@Entity
+public class Department {
+    private Long id;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+    private String departmentName;
+    private Long pid;
+    private Byte enabled;
+
+    @Id
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "create_time")
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "update_time")
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Basic
+    @Column(name = "department_name")
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Basic
+    @Column(name = "pid")
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    @Basic
+    @Column(name = "enabled")
+    public Byte getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Byte enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(createTime, that.createTime) &&
+                Objects.equals(updateTime, that.updateTime) &&
+                Objects.equals(departmentName, that.departmentName) &&
+                Objects.equals(pid, that.pid) &&
+                Objects.equals(enabled, that.enabled);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createTime, updateTime, departmentName, pid, enabled);
+    }
+}
