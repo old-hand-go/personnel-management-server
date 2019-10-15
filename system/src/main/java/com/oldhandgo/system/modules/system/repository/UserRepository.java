7c395428a01a464574a25fa6884ac6239401864c
dormirr <1018729292@qq.com>
2019-10-15 20:52:37 +0800

    保证查询结果唯一。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/system/repository/UserRepository.java
+++ system/src/main/java/com/oldhandgo/system/modules/system/repository/UserRepository.java
@@ -4,15 +4,19 @@ import com.oldhandgo.system.modules.system.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 /**
  * @author dormirr
  */
 public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
+
     /**
      * 根据邮箱查询
      *
-     * @param email 邮箱
+     * @param email     邮箱
+     * @param isEnabled 开启的用户 必为1
      * @return 查询结果
      */
-    User findByEmail(String email);
+    Optional<User> findByEmailAndIsEnabled(String email, Byte isEnabled);
 }
