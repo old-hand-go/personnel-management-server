43b55e7a99256e4d3587038542c51ba760f08331
dormirr <1018729292@qq.com>
2019-10-15 10:32:08 +0800

    创建安全工具类。
    Signed-off-by: dormirr <1018729292@qq.com>

---
+++ common/src/main/java/com/oldhandgo/common/utils/SecurityUtils.java
@@ -0,0 +1,55 @@
+package com.oldhandgo.common.utils;
+
+
+import cn.hutool.json.JSONObject;
+import com.oldhandgo.common.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @author dormirr
+ */
+public class SecurityUtils {
+    /**
+     * @return 用户详细信息
+     */
+    public static UserDetails getUserDetails() {
+        UserDetails userDetails = null;
+        try {
+            userDetails = (UserDetails) org
+                    .springframework
+                    .security
+                    .core
+                    .context
+                    .SecurityContextHolder
+                    .getContext()
+                    .getAuthentication()
+                    .getPrincipal();
+        } catch (Exception e) {
+            throw new BadRequestException(HttpStatus.UNAUTHORIZED, "登录状态过期");
+        }
+        return userDetails;
+    }
+
+    /**
+     * 获取系统用户名称
+     *
+     * @return 系统用户名称
+     */
+    public static String getUserName() {
+        Object obj = getUserDetails();
+        JSONObject json = new JSONObject(obj);
+        return json.get("username", String.class);
+    }
+
+    /**
+     * 获取系统用户ID
+     *
+     * @return 系统用户ID
+     */
+    public static Long getUserId() {
+        Object obj = getUserDetails();
+        JSONObject json = new JSONObject(obj);
+        return json.get("id", Long.class);
+    }
+}
