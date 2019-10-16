e1179847e52a9a380345468a49395a8b1fbbffd0
dormirr <1018729292@qq.com>
2019-10-16 19:38:21 +0800

    修改查询注解。
    Signed-off-by: dormirr <1018729292@qq.com>

--- common/src/main/java/com/oldhandgo/common/annotation/Query.java
+++ common/src/main/java/com/oldhandgo/common/annotation/Query.java
@@ -5,62 +5,53 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 /**
- * 查询
+ * 查询注解
  *
  * @author dormirr
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Query {
-
-    /**
-     * 基本对象的属性名
-     */
+    // 基本对象的属性名
     String propName() default "";
 
-    /**
-     * 查询方式
-     */
+    // 查询方式
     Type type() default Type.EQUAL;
 
-    /**
-     * 连接查询的属性名，如User类中的dept
-     */
+    // 连接查询的属性名，如User类中的dept
     String joinName() default "";
 
-    /**
-     * 默认左连接
-     */
+    // 默认左连接
     Join join() default Join.LEFT;
 
-    /**
-     * 多字段模糊搜索，仅支持String类型字段，多个用逗号隔开, 如@Query(blurry = "email,username")
-     */
+    // 多字段模糊搜索，仅支持String类型字段，多个用逗号隔开, 如@Query(blurry = "email,username")\
     String blurry() default "";
 
     enum Type {
-        /**
-         * 相等
-         */
-        EQUAL
-        /* 大于等于 */, GREATER_THAN
-        /* 小于等于 */, LESS_THAN
-        /* 中模糊查询 */, INNER_LIKE
-        /* 左模糊查询 */, LEFT_LIKE
-        /* 右模糊查询 */, RIGHT_LIKE
-        /* 小于 */, LESS_THAN_NQ
-        /* 包含 */, IN
+        // 相等
+        EQUAL,
+        // 大于等于
+        GREATER_THAN,
+        // 小于等于
+        LESS_THAN,
+        // 中模糊查询
+        INNER_LIKE,
+        // 左模糊查询
+        LEFT_LIKE,
+        // 右模糊查询
+        RIGHT_LIKE,
+        // 小于
+        LESS_THAN_NQ,
+        // 包含
+        IN
     }
 
-    // 适用于简单连接查询，复杂的请自定义该注解，或者使用sql查询
     enum Join {
-        /*左连接*/
+        // 左连接
         LEFT,
-        /* 右连接 */
+        // 右连接
         RIGHT
     }
-
 }
 
