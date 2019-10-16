4f03ee6c123bb85a07df7eb4b341bc78a5ae4a17
dormirr <1018729292@qq.com>
2019-10-16 21:03:43 +0800

    添加验证空的方法。
    Signed-off-by: dormirr <1018729292@qq.com>

--- common/src/main/java/com/oldhandgo/common/utils/ValidationUtils.java
+++ common/src/main/java/com/oldhandgo/common/utils/ValidationUtils.java
@@ -1,5 +1,9 @@
 package com.oldhandgo.common.utils;
 
+import com.oldhandgo.common.exception.BadRequestException;
+
+import java.util.Optional;
+
 /**
  * @author dormir
  */
@@ -16,5 +20,22 @@ public class ValidationUtils {
         }
         String regEx1 = "^([a-z0-9A-Z]+[-|.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
         return string.matches(regEx1);
+    }
+
+    /**
+     * 验证空
+     *
+     * @param optional  实体类
+     * @param entity    实体类名
+     * @param parameter 类
+     * @param value     值
+     */
+    public static void isNull(Optional optional, String entity, String parameter, Object value) {
+        if (optional.isEmpty()) {
+            String msg = entity
+                    + " 不存在 "
+                    + "{ " + parameter + ":" + value.toString() + " }";
+            throw new BadRequestException(msg);
+        }
     }
 }
