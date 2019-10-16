b3bd4ef204657208370d71b2489020853a80fe5a
dormirr <1018729292@qq.com>
2019-10-16 14:48:46 +0800

    修改测试类。
    Signed-off-by: dormirr <1018729292@qq.com>

--- common/src/test/java/com/oldhandgo/common/CommonApplicationTests.java
+++ common/src/test/java/com/oldhandgo/common/CommonApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.DigestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +14,9 @@ public class CommonApplicationTests {
     public void contextLoads() {
     }
 
+    @Test
+    public void encryptPassword() {
+        String password = "123456";
+        System.out.println(DigestUtils.md5DigestAsHex(password.getBytes()));
+    }
 }
--- system/src/test/java/com/oldhandgo/system/SystemApplicationTests.java
+++ system/src/test/java/com/oldhandgo/system/SystemApplicationTests.java
@@ -1,16 +1,39 @@
 package com.oldhandgo.system;
 
+import com.oldhandgo.common.utils.ValidationUtils;
+import com.oldhandgo.system.modules.system.domain.User;
+import com.oldhandgo.system.modules.system.repository.UserRepository;
+import com.oldhandgo.system.modules.system.service.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SystemApplicationTests {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserMapper userMapper;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void findByEmail() {
+        String email = "1018729292@qq.com";
+        Byte isEnabled = 1;
+        User user = null;
+        if (ValidationUtils.isEmail(email)) {
+            user = userRepository.findByEmailAndIsEnabled(email, isEnabled).orElse(null);
+        }
+        if (user != null) {
+            System.out.println(userMapper.userToUserDto(user));
+        } else {
+            System.out.println("空");
+        }
+    }
 }
