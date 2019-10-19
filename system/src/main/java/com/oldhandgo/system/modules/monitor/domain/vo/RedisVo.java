3dc5abe394352bfa3a2b4b3183db24bbe9da6372
dormirr <1018729292@qq.com>
2019-10-19 16:46:36 +0800

    创建访客实体类与视图模型。
    Signed-off-by: dormirr <1018729292@qq.com>

---
+++ system/src/main/java/com/oldhandgo/system/modules/monitor/domain/vo/RedisVo.java
@@ -0,0 +1,23 @@
+package com.oldhandgo.system.modules.monitor.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+/**
+ * @author dormirr
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RedisVo implements Serializable {
+
+    @NotBlank
+    private String key;
+
+    @NotBlank
+    private String value;
+}
