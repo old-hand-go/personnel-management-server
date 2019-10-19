4f79784fb84a285be477d018ad2e2e4571ba77d0
dormirr <1018729292@qq.com>
2019-10-19 16:28:04 +0800

    添加Redis缓存。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/security/rest/AuthenticationController.java
+++ system/src/main/java/com/oldhandgo/system/modules/security/rest/AuthenticationController.java
@@ -2,8 +2,11 @@ package com.oldhandgo.system.modules.security.rest;
 
 import cn.hutool.core.codec.Base64;
 import cn.hutool.core.util.IdUtil;
+import com.oldhandgo.common.exception.BadRequestException;
 import com.oldhandgo.common.utils.EncryptUtils;
 import com.oldhandgo.common.utils.SecurityUtils;
+import com.oldhandgo.common.utils.StringUtils;
+import com.oldhandgo.system.modules.monitor.service.RedisService;
 import com.oldhandgo.system.modules.security.security.AuthenticationInfo;
 import com.oldhandgo.system.modules.security.security.AuthorizationUser;
 import com.oldhandgo.system.modules.security.security.ImgResult;
@@ -12,6 +15,7 @@ import com.oldhandgo.system.modules.security.service.impl.JwtUserDetailsServiceI
 import com.oldhandgo.system.modules.security.utils.JwtTokenUtils;
 import com.oldhandgo.system.modules.security.utils.VerifyCodeUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AccountExpiredException;
@@ -34,10 +38,12 @@ public class AuthenticationController {
     private String tokenHeader;
     private final JwtTokenUtils jwtTokenUtils;
     private final JwtUserDetailsServiceImpl jwtUserDetailsServiceImpl;
+    private final RedisService redisService;
 
-    public AuthenticationController(JwtTokenUtils jwtTokenUtils, JwtUserDetailsServiceImpl jwtUserDetailsServiceImpl) {
+    public AuthenticationController(JwtTokenUtils jwtTokenUtils, JwtUserDetailsServiceImpl jwtUserDetailsServiceImpl, RedisService redisService) {
         this.jwtTokenUtils = jwtTokenUtils;
         this.jwtUserDetailsServiceImpl = jwtUserDetailsServiceImpl;
+        this.redisService = redisService;
     }
 
     /**
@@ -46,18 +52,19 @@ public class AuthenticationController {
      * @param authorizationUser 授权用户
      * @return 授权
      */
+    @Log("用户登录")
     @PostMapping(value = "${jwt.auth.path}")
     public ResponseEntity login(@Validated @RequestBody AuthorizationUser authorizationUser) {
-//        // 查询验证码
-//        String code = redisService.getCodeVal(authorizationUser.getUuid());
-//        // 清除验证码
-//        redisService.delete(authorizationUser.getUuid());
-//        if (StringUtils.isBlank(code)) {
-//            throw new BadRequestException("验证码已过期");
-//        }
-//        if (StringUtils.isBlank(authorizationUser.getCode()) || !authorizationUser.getCode().equalsIgnoreCase(code)) {
-//            throw new BadRequestException("验证码错误");
-//        }
+        // 查询验证码
+        String code = redisService.getCodeVal(authorizationUser.getUuid());
+        // 清除验证码
+        redisService.delete(authorizationUser.getUuid());
+        if (StringUtils.isBlank(code)) {
+            throw new BadRequestException("验证码已过期");
+        }
+        if (StringUtils.isBlank(authorizationUser.getCode()) || !authorizationUser.getCode().equalsIgnoreCase(code)) {
+            throw new BadRequestException("验证码错误");
+        }
         final JwtUser jwtUser = (JwtUser) jwtUserDetailsServiceImpl.loadUserByUsername(authorizationUser.getUsername());
         if (!jwtUser.getPassword().equals(EncryptUtils.encryptPassword(authorizationUser.getPassword()))) {
             throw new AccountExpiredException("密码错误");
