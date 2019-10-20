9cb82fd1469925ca27d71c8212ffb794b7de25f7
dormirr <1018729292@qq.com>
2019-10-20 20:29:00 +0800

    增加IP方法。
    Signed-off-by: dormirr <1018729292@qq.com>

--- common/src/main/java/com/oldhandgo/common/utils/StringUtils.java
+++ common/src/main/java/com/oldhandgo/common/utils/StringUtils.java
@@ -1,5 +1,13 @@
 package com.oldhandgo.common.utils;
 
+import cn.hutool.core.io.resource.ClassPathResource;
+import org.lionsoul.ip2region.DataBlock;
+import org.lionsoul.ip2region.DbConfig;
+import org.lionsoul.ip2region.DbSearcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.lang.reflect.Method;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -116,6 +124,68 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
         return sb.toString();
     }
 
+    /**
+     * 获取ip地址
+     *
+     * @param request
+     * @return
+     */
+    public static String getIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        String[] ips = ip.split(",");
+        return "0:0:0:0:0:0:0:1".equals(ips[0]) ? "127.0.0.1" : ips[0];
+    }
+
+    /**
+     * 根据ip获取详细地址
+     *
+     * @param ip
+     * @return
+     */
+    public static String getCityInfo(String ip) {
+        try {
+            String path = "ip2region/ip2region.db";
+            String name = "ip2region.db";
+            int algorithm = DbSearcher.BTREE_ALGORITHM;
+            DbConfig config = new DbConfig();
+            File file = FileUtils.inputStreamToFile(new ClassPathResource(path).getStream(), name);
+            DbSearcher searcher = new DbSearcher(config, file.getPath());
+            Method method = null;
+            switch (algorithm) {
+                case DbSearcher.BTREE_ALGORITHM:
+                    method = searcher.getClass().getMethod("btreeSearch", String.class);
+                    break;
+                case DbSearcher.BINARY_ALGORITHM:
+                    method = searcher.getClass().getMethod("binarySearch", String.class);
+                    break;
+                case DbSearcher.MEMORY_ALGORITYM:
+                    method = searcher.getClass().getMethod("memorySearch", String.class);
+                    break;
+                default:
+                    method = searcher.getClass().getMethod("memorySearch", String.class);
+                    break;
+            }
+            DataBlock dataBlock = null;
+            dataBlock = (DataBlock) method.invoke(searcher, ip);
+            String address = dataBlock.getRegion().replace("0|", "");
+            if (address.charAt(address.length() - 1) == '|') {
+                address = address.substring(0, address.length() - 1);
+            }
+            return address.equals(ElAdminConstant.REGION) ? "内网IP" : address;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
     /**
      * 获得当天是周几
      *
