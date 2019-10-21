dadf8569c4f5e1db07bd8beee733934252094200
dormirr <1018729292@qq.com>
2019-10-21 15:27:20 +0800

    优化工具类。
    Signed-off-by: dormirr <1018729292@qq.com>

--- common/src/main/java/com/oldhandgo/common/utils/SecurityUtils.java
+++ common/src/main/java/com/oldhandgo/common/utils/SecurityUtils.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
+ * 获取当前登录的用户
+ *
  * @author dormirr
  */
 public class SecurityUtils {
@@ -16,15 +18,7 @@ public class SecurityUtils {
     public static UserDetails getUserDetails() {
         UserDetails userDetails;
         try {
-            userDetails = (UserDetails) org
-                    .springframework
-                    .security
-                    .core
-                    .context
-                    .SecurityContextHolder
-                    .getContext()
-                    .getAuthentication()
-                    .getPrincipal();
+            userDetails = (UserDetails) org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         } catch (Exception e) {
             throw new BadRequestException(HttpStatus.UNAUTHORIZED, "登录状态过期");
         }
