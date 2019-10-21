19541383bf76df8e023797f6ed2e88cbd13f15b5
dormirr <1018729292@qq.com>
2019-10-21 16:31:58 +0800

    修改数据库。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/monitor/domain/Visits.java
+++ system/src/main/java/com/oldhandgo/system/modules/monitor/domain/Visits.java
@@ -1,103 +1,36 @@
 package com.oldhandgo.system.modules.monitor.domain;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.Objects;
 
 /**
  * @author dormirr
  */
 @Entity
+@Data
+@Table(name = "visits")
 public class Visits {
-    private Long id;
-    private Timestamp createTime;
-    private Timestamp updateTime;
-    private Long ipCounts;
-    private Long pvCounts;
-    private String weekDay;
 
     @Id
-    @Column(name = "id")
-    public Long getId() {
-        return id;
-    }
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(unique = true)
+    private String date;
 
-    @Basic
-    @Column(name = "create_time")
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
-    @Basic
-    @Column(name = "update_time")
-    public Timestamp getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Timestamp updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Basic
-    @Column(name = "ip_counts")
-    public Long getIpCounts() {
-        return ipCounts;
-    }
-
-    public void setIpCounts(Long ipCounts) {
-        this.ipCounts = ipCounts;
-    }
-
-    @Basic
     @Column(name = "pv_counts")
-    public Long getPvCounts() {
-        return pvCounts;
-    }
+    private Long pvCounts;
 
-    public void setPvCounts(Long pvCounts) {
-        this.pvCounts = pvCounts;
-    }
+    @Column(name = "ip_counts")
+    private Long ipCounts;
 
-    @Basic
+    @CreationTimestamp
+    @Column(name = "create_time")
+    private Timestamp createTime;
+
     @Column(name = "week_day")
-    public String getWeekDay() {
-        return weekDay;
-    }
-
-    public void setWeekDay(String weekDay) {
-        this.weekDay = weekDay;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Visits visits = (Visits) o;
-        return Objects.equals(id, visits.id) &&
-                Objects.equals(createTime, visits.createTime) &&
-                Objects.equals(updateTime, visits.updateTime) &&
-                Objects.equals(ipCounts, visits.ipCounts) &&
-                Objects.equals(pvCounts, visits.pvCounts) &&
-                Objects.equals(weekDay, visits.weekDay);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, createTime, updateTime, ipCounts, pvCounts, weekDay);
-    }
+    private String weekDay;
 }
