b93f16ce3e1a8ea93245ef2d1471889458212403
dormirr <1018729292@qq.com>
2019-10-21 16:54:30 +0800

    修改业务逻辑处理类JwtUserDetailsServiceImpl。
    Signed-off-by: dormirr <1018729292@qq.com>

--- system/src/main/java/com/oldhandgo/system/modules/security/service/impl/JwtUserDetailsServiceImpl.java
+++ system/src/main/java/com/oldhandgo/system/modules/security/service/impl/JwtUserDetailsServiceImpl.java
@@ -3,6 +3,8 @@ package com.oldhandgo.system.modules.security.service.impl;
 import com.oldhandgo.common.exception.BadRequestException;
 import com.oldhandgo.system.modules.security.security.JwtUser;
 import com.oldhandgo.system.modules.system.service.UserService;
+import com.oldhandgo.system.modules.system.service.dto.DeptSmallDTO;
+import com.oldhandgo.system.modules.system.service.dto.JobSmallDTO;
 import com.oldhandgo.system.modules.system.service.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +12,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * JWT用户详细类Impl
@@ -20,14 +24,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true, rollbackFor = Exception.class)
 public class JwtUserDetailsServiceImpl implements UserDetailsService {
     private final UserService userService;
+    private final JwtPermissionServiceImpl permissionService;
 
-    public JwtUserDetailsServiceImpl(UserService userService) {
+    public JwtUserDetailsServiceImpl(UserService userService, JwtPermissionServiceImpl permissionService) {
         this.userService = userService;
+        this.permissionService = permissionService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        UserDTO userDTO = userService.findByEmailAndIsEnabled(userName, (byte) 1);
+        UserDTO userDTO = userService.findByName(userName);
         if (userDTO == null) {
             throw new BadRequestException("账号不存在");
         } else {
@@ -35,18 +41,20 @@ public class JwtUserDetailsServiceImpl implements UserDetailsService {
         }
     }
 
-    public UserDetails createJwtUser(UserDTO userDTO) {
+    public UserDetails createJwtUser(UserDTO user) {
         return new JwtUser(
-                userDTO.getId(),
-                userDTO.getCreateTime(),
-                userDTO.getUpdateTime(),
-                userDTO.getEmail(),
-                userDTO.getUserName(),
-                userDTO.getPassWord(),
-                userDTO.getAddress(),
-                userDTO.getDeptId(),
-                userDTO.getJobId(),
-                userDTO.getIsEnabled()
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getAvatar(),
+                user.getEmail(),
+                user.getPhone(),
+                Optional.ofNullable(user.getDept()).map(DeptSmallDTO::getName).orElse(null),
+                Optional.ofNullable(user.getJob()).map(JobSmallDTO::getName).orElse(null),
+                permissionService.mapToGrantedAuthorities(user),
+                user.getEnabled(),
+                user.getCreateTime(),
+                user.getLastPasswordResetTime()
         );
     }
 }
